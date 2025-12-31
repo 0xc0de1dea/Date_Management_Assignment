8e316d84d3937494f40da10eaa79cd7d4e3c518c
0xc0de1dea <0xc0de1dea@gmail.com>
2025-12-31 11:39:01 +0900

    feat: 도전기능-유저의 입력에 대한 검증 구현

--- src/main/java/com/example/routine/entity/Comment.java
+++ src/main/java/com/example/routine/entity/Comment.java
@@ -13,8 +13,11 @@ public class Comment extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 100, nullable = false)
     private String content;
+    @Column(nullable = false)
     private String author;
+    @Column(nullable = false)
     private String password;
 
     @ManyToOne
--- src/main/java/com/example/routine/entity/Date.java
+++ src/main/java/com/example/routine/entity/Date.java
@@ -15,9 +15,13 @@ public class Date extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length = 30, nullable = false)
     private String name;
+    @Column(length = 200, nullable = false)
     private String content;
+    @Column(nullable = false)
     private String author;
+    @Column(nullable = false)
     private String password;
 
     @OneToMany(mappedBy = "date")
